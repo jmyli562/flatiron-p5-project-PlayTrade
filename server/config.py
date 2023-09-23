ed3e95c9fc313fb45746b1475b955a9a6a805d78
Jimmy Li <jmyli562@gmail.com>
2023-09-22 20:57:24 -0700

    added secret_key to bcrypt

--- server/config.py
+++ server/config.py
@@ -8,11 +8,13 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-
+import os
+from dotenv import load_dotenv
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
