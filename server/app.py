603ad5eb805dc7e650ec13944220b33d4592622e
Jimmy Li <jmyli562@gmail.com>
2023-09-23 14:04:15 -0700

    add models for user, game, review, and comment

--- server/app.py
+++ server/app.py
@@ -3,21 +3,22 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
+from models import User
 
+# Add your model imports
 
 # Views go here!
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Project Server</h1>'
+    return "<h1>Project Server</h1>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -1,6 +1,60 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+game_library = db.Table(
+    "game_library",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("game_id", db.Integer, db.ForeignKey("games.id")),
+)
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String)
+    library = db.relationship(
+        "Game", secondary=game_library, back_populates="owners", lazy="dynamic"
+    )
+    review = db.relationship("Review", backref="user")
+    points = db.Column(db.Integer)
+    _password_hash = db.Column(db.String)
+
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    posted_at = db.Column(db.Date)
+    updated_at = db.Column(db.Date)
+
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    release_date = db.Column(db.Date)
+    image_url = db.Column(db.String)
+    developer = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.Integer)
+    owners = db.relationship(
+        "User", secondary=game_library, back_populates="library", lazy="dynamic"
+    )
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
