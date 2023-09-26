897b67d828f0065762b5dd84a2ee195e02c7dd3b
Jimmy Li <jmyli562@gmail.com>
2023-09-26 03:45:01 -0700

    modified starRating component

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -4,26 +4,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 
 function StarRating({ rating }) {
+  // Round the rating down to the nearest half star
+  const roundedRating = Math.round(rating * 2) / 2;
+  const maxStars = 5;
   const stars = [];
-  const maxRating = 5;
-
-  for (let i = 1; i <= maxRating; i++) {
-    if (i <= rating) {
-      stars.push(
-        <FontAwesomeIcon icon={faStar} key={i} className="filled-star" />
-      );
-    } else if (i - 0.5 === rating) {
-      stars.push(
-        <FontAwesomeIcon icon={faStarHalfAlt} key={i} className="half-star" />
-      );
-    } else {
-      stars.push(
-        <FontAwesomeIcon icon={faStar} key={i} className="empty-star" />
-      );
-    }
+  //adding all the filled whole stars to the stars array
+  let i = roundedRating;
+  for (i; i >= 1; i--) {
+    stars.push(<FontAwesomeIcon icon={faStar} className="filled-star" />);
   }
 
-  return <div className="star-rating">Rating: {stars}</div>;
+  //if there exists a half star append it
+  if (i == 0.5) {
+    stars.push(<FontAwesomeIcon icon={faStarHalfAlt} className="half-star" />);
+  }
+  //filling in the empty stars
+  for (let i = maxStars - rating; i >= 1; i--) {
+    stars.push(<FontAwesomeIcon icon={faStar} className="empty-star" />);
+  }
+
+  return (
+    <div className="star-rating">
+      <p>{stars}</p>
+      <p>{rating}</p>
+    </div>
+  );
 }
 
 export default StarRating;
