1ed7802eaa8971a34087c7eac08b81ea53ffbcdc
Jimmy Li <jmyli562@gmail.com>
2023-09-26 01:41:27 -0700

    added search filtering for game list

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -3,9 +3,24 @@ import "../components/css/GameList.css";
 import GameCard from "./GameCard";
 function GameList({ allgames }) {
   const [titleFilter, setTitleFilter] = useState("");
+  const [sortFilter, setSortFilter] = useState("");
   const filteredGames = allgames.filter((game) =>
     game.name.toLowerCase().includes(titleFilter.toLowerCase())
   );
+  filteredGames.sort((a, b) => {
+    console.log(sortFilter);
+    if (sortFilter === "AtoZ") {
+      return a.name.localeCompare(b.name);
+    } else if (sortFilter === "ZtoA") {
+      return b.name.localeCompare(a.name);
+    } else if (sortFilter === "rating") {
+      return b.rating - a.rating;
+    } else if (sortFilter === "releaseDate") {
+      return new Date(b.release_date) - new Date(a.release_date);
+    } else {
+      return 0;
+    }
+  });
   return (
     <div style={{ backgroundColor: "white" }}>
       <div className="search-bar">
@@ -15,9 +30,13 @@ function GameList({ allgames }) {
           value={titleFilter}
           onChange={(e) => setTitleFilter(e.target.value)}
         />
-        <select>
+        <select
+          value={sortFilter}
+          onChange={(e) => setSortFilter(e.target.value)}
+        >
           <option value="">Sort by</option>
-          <option value="rating">Name A-Z</option>
+          <option value="AtoZ">Name A-Z</option>
+          <option value="ZtoA">Name Z-A</option>
           <option value="releaseDate">Release Date</option>
           <option value="rating">Rating</option>
         </select>
