46372075d9ed25924eff18a376db882e820e7c4c
Jimmy Li <jmyli562@gmail.com>
2023-09-26 12:37:32 -0700

    add formik and validations to GameReview component

--- client/src/components/GameReview.js
+++ client/src/components/GameReview.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "../components/css/GameReview.css";
+import { useFormik } from "formik";
 //if the game does not have a review.... the title will be Leave a Review for {game}
 //be sure to include back button to redirect back to /games
 //background image will be the game image
+//if the user is not logged in currently, do not allow to create a review, check isLoggedIn state
 function GameReview({ game }) {
-  console.log(game);
+  //console.log(game);
+  const formik = useFormik({
+    initialValues: {
+      rating: 0,
+      content: "",
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.rating) {
+        errors.rating = "Rating must not be blank.";
+      }
+      if (!values.content) {
+        errors.content = "Review must not be blank";
+      }
+
+      return errors;
+    },
+  });
   return (
     <div className="game-review">
       <h1>Leave a Review for {game.name}</h1>
@@ -15,11 +34,34 @@ function GameReview({ game }) {
       <form>
         <div className="rating">
           <label>Rating:</label>
-          <input type="number" step="0.5" min="0" max="5" />
+          <input
+            type="number"
+            step="0.5"
+            min="0"
+            max="5"
+            id="rating"
+            name="rating"
+            value={formik.values.rating}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
         </div>
+        {formik.touched.rating && formik.errors.rating ? (
+          <div className="error">{formik.errors.rating}</div>
+        ) : null}
         <div className="review-text">
           <label>Review:</label>
-          <textarea />
+          <textarea
+            type="text"
+            id="content"
+            name="content"
+            value={formik.values.content}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.content && formik.errors.content ? (
+            <div className="error">{formik.errors.content}</div>
+          ) : null}
         </div>
         <button className="review-button" type="submit">
           Submit
