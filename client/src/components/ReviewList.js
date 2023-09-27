3f61025261aadca679bd98c3a8b807aabc645766
Jimmy Li <jmyli562@gmail.com>
2023-09-27 12:51:42 -0700

    add styling to ReviewList

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "../components/css/ReviewList.css";
 import StarRating from "./StarRating";
