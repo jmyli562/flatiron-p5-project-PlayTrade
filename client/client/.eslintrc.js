414d8fb6924bd0075537fe2c5f9950c8d9f2d8c8
Jimmy Li <jmyli562@gmail.com>
2023-10-08 14:51:13 -0700

    added custom configuration file

---
+++ client/client/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Your existing ESLint rules...
+  rules: {
+    // ...other rules
+    "array-callback-return": "off", // Disable this rule
+    "react-hooks/exhaustive-deps": "off", // Disable this rule
+  },
+};
